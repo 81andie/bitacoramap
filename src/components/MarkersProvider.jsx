8ac6a30e33c71dc431bb144b48570e5c352e37be
81andie <133395577+81andie@users.noreply.github.com>
2024-08-28 11:52:09 +0200

    cambios funcion guardar localStorage

--- src/components/MarkersProvider.jsx
+++ src/components/MarkersProvider.jsx
@@ -1,42 +1,53 @@
 
-import React, { useState , useContext} from "react";
+import React, { useState, useContext, useEffect } from "react";
 
 
 
 const markerContext = React.createContext();
 const storeMarkersContext = React.createContext();
 
-export function useMarkerContext (){
+export function useMarkerContext() {
     return useContext(markerContext);
 }
 
-export function useStoreMarkersContext (){
+export function useStoreMarkersContext() {
     return useContext(storeMarkersContext);
 }
 
-export function MarkersProvider({children}) {
+export function MarkersProvider({ children }) {
 
     const [markers, setMarkers] = useState([]);
 
-    const storeMarker = (coordinate) => {
-        
-      setMarkers(prevCoordenadas => {
-            const nuevoArrayCoord = [...prevCoordenadas, coordinate];
-            console.log(nuevoArrayCoord)
-            localStorage.setItem("listaMarkers", JSON.stringify(nuevoArrayCoord))
-            return nuevoArrayCoord;
-        });
-      
-      
- 
-  
+    const storeMarker = (coordinate = null) => {
 
-}
+        /* setMarkers(prevCoordenadas => {
+               const nuevoArrayCoord = [...prevCoordenadas, coordinate];
+               console.log(nuevoArrayCoord)
+               localStorage.setItem("listaMarkers", JSON.stringify(nuevoArrayCoord))
+               return nuevoArrayCoord;
+           });*/
+
+        let storedMarkers = localStorage.getItem("listaMarkers");
+        let updatedMarkers = storedMarkers ? JSON.parse(storedMarkers) : [];
+
+        if (coordinate) {
+            updatedMarkers = [...updatedMarkers, coordinate];
+            localStorage.setItem("listaMarkers", JSON.stringify(updatedMarkers));
+        }
+
+        setMarkers(updatedMarkers);
+
+    }
+
+    useEffect(() => {
+        storeMarker();
+    }, []);
+
 
 
     return (
 
-        <markerContext.Provider value={{markers}}>
+        <markerContext.Provider value={{ markers }}>
             <storeMarkersContext.Provider value={storeMarker}>
                 {children}
             </storeMarkersContext.Provider>
